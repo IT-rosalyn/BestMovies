9ada438e92eb3db4743f3822b30990032b4b34d7
Jiajun-Jiang <87977142+Jiajun-Jiang@users.noreply.github.com>
2021-08-05 18:05:31 +0800

    Update admin.py

--- Best_movies/admin.py
+++ Best_movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from rango.models import Movie, FavoriteList, UserProfile,Review,
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(UserProfile)
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(FavoriteList)
