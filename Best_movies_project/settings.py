36aaf8af35dafcf1a463270c051208430963f0b6
Lena <834480718@qq.com>
2021-08-05 17:27:04 +0800

    settingEnv

--- Best_movies_project/settings.py
+++ Best_movies_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Best_movies',
+    'Best_movies.apps.BestMoviesConfig',
 ]
 
 MIDDLEWARE = [
